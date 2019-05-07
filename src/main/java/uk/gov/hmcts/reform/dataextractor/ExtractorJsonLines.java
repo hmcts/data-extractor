52442adfa79f1dc0cc40b1642fe9b1a650968b9d
luigibk <41112628+luigibk@users.noreply.github.com>
2019-05-07 14:36:44 +0100

    Add jsonlines support (#2)

--- src/main/java/uk/gov/hmcts/reform/dataextractor/ExtractorJsonLines.java
+++ src/main/java/uk/gov/hmcts/reform/dataextractor/ExtractorJsonLines.java
@@ -1,48 +1,39 @@
 package uk.gov.hmcts.reform.dataextractor;
 
-import com.fasterxml.jackson.core.JsonEncoding;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.util.MinimalPrettyPrinter;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 
-public class ExtractorJson implements Extractor {
-
-    public void apply(ResultSet resultSet, OutputStream outputStream) {
-        final ObjectMapper objectMapper = new ObjectMapper();
-        try (JsonGenerator jsonGenerator =
-            objectMapper.getFactory().createGenerator(outputStream, JsonEncoding.UTF8)
-        ) {
-            writeResultSetToJson(resultSet, jsonGenerator);
-            jsonGenerator.flush();
-        } catch (IOException | SQLException e) {
-            throw new ExtractorException(e);
-        }
-    }
+public class ExtractorJsonLines extends ExtractorJson {
 
     protected void writeResultSetToJson(ResultSet resultSet, JsonGenerator jsonGenerator)
         throws SQLException, IOException {
         final ResultSetMetaData metaData = resultSet.getMetaData();
         final int columnCount = metaData.getColumnCount();
-        jsonGenerator.writeStartArray();
+        jsonGenerator.setPrettyPrinter(new MinimalPrettyPrinter(""));
         while (resultSet.next()) {
             jsonGenerator.writeStartObject();
             for (int i = 1; i <= columnCount; i++) {
                 writeJsonLine(jsonGenerator, metaData.getColumnName(i), resultSet.getObject(i), metaData.getColumnTypeName(i));
             }
             jsonGenerator.writeEndObject();
+            jsonGenerator.writeRaw('\n');
         }
-        jsonGenerator.writeEndArray();
     }
 
     protected void writeJsonLine(JsonGenerator jsonGenerator, String columnName, Object data, String dataType)
         throws IOException {
-        jsonGenerator.writeObjectField(columnName, data);
+        if (dataType.contains("json")) {
+            jsonGenerator.writeFieldName(columnName);
+            jsonGenerator.writeRawValue(data.toString());
+        } else {
+            jsonGenerator.writeObjectField(columnName, data);
+        }
     }
 
 }
