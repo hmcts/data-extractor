054f82623006b3e42652a6f5861ccb9f6a1c58f1
Qiang Zhou <qiang.zhou@hmcts.net>
2019-11-27 15:25:24 +0000

    Added application config test

--- src/main/java/uk/gov/hmcts/reform/dataextractor/config/ApplicationConfig.java
+++ src/main/java/uk/gov/hmcts/reform/dataextractor/config/ApplicationConfig.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.dataextractor.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 import uk.gov.hmcts.reform.dataextractor.BlobOutputWriter;
 import uk.gov.hmcts.reform.dataextractor.DataExtractorApplication;
 import uk.gov.hmcts.reform.dataextractor.Extractor;
@@ -25,32 +24,28 @@ public class ApplicationConfig {
 
     @Bean
     public Factory<ExtractionData, BlobOutputWriter> blobOutputFactory() {
-        return config -> blobOutputWriter(config);
+        return this::blobOutputWriter;
     }
 
     @Bean
     public Factory<String,  QueryExecutor> queryExecutorFactory() {
-        return sqlQuery -> blobOutputWriter(sqlQuery);
+        return this::blobOutputWriter;
     }
 
     @Bean
     public Factory<DataExtractorApplication.Output, Extractor> extractorFactory() {
-        return outputType -> extractor(outputType);
+        return this::extractor;
     }
 
-    @Bean
-    @Scope(value = "prototype")
-    public BlobOutputWriter blobOutputWriter(ExtractionData config) {
+    private BlobOutputWriter blobOutputWriter(ExtractionData config) {
         return new BlobOutputWriter(config.getContainer(), config.getPrefix(), config.getType(), outputStreamProvider);
     }
 
-    @Bean
-    @Scope(value = "prototype")
-    public QueryExecutor blobOutputWriter(String sqlQuery) {
+    private QueryExecutor blobOutputWriter(String sqlQuery) {
         return new QueryExecutor(dbConfig.getUrl(), dbConfig.getUser(), dbConfig.getPassword(), sqlQuery);
     }
 
-    public Extractor extractor(DataExtractorApplication.Output outputType) {
+    private Extractor extractor(DataExtractorApplication.Output outputType) {
         switch (outputType) {
             case JSON_LINES: return new ExtractorJsonLines();
             case JSON: return new ExtractorJson();
