ee714345809fa74db8cea044bd18cff0969421ed
Qiang Zhou <qiang.zhou@hmcts.net>
2019-12-04 14:31:42 +0000

    Fix coverage

--- src/test/java/uk/gov/hmcts/reform/dataextractor/config/ApplicationConfigTest.java
+++ src/test/java/uk/gov/hmcts/reform/dataextractor/config/ApplicationConfigTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import uk.gov.hmcts.reform.dataextractor.BlobOutputWriter;
 import uk.gov.hmcts.reform.dataextractor.DataExtractorApplication;
@@ -14,6 +15,7 @@ import uk.gov.hmcts.reform.dataextractor.QueryExecutor;
 import java.sql.Connection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -24,6 +26,9 @@ public class ApplicationConfigTest {
 
     @Mock
     private Connection connection;
+
+    @Spy
+    private DbConfig dbConfig;
 
     @InjectMocks
     private ApplicationConfig classToTest;
@@ -52,5 +57,14 @@ public class ApplicationConfigTest {
         QueryExecutor result = queryExecutorFactory.provide("sqlQuery");
         QueryExecutor expected = new QueryExecutor(connection, "sqlQuery");
         assertThat(result).isEqualToComparingFieldByField(expected);
+    }
+
+    @Test
+    public void givenDBConnectionError_thenThrowException() {
+        dbConfig.setBaseDir("BaseDir");
+        dbConfig.setPassword("password");
+        dbConfig.setUrl("ulr");
+        dbConfig.setUser("user");
+        assertThrows(IllegalStateException.class, () -> connection = classToTest.dbConnection());
     }
 }
