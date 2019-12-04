78661da1873b0c76e461f2b611288c8d83244221
qzhou-hmcts <42572376+qzhou-hmcts@users.noreply.github.com>
2019-12-04 16:11:01 +0000

    Setup vault and integration test config (#35)
    * Added missing library
    
    * temporal suppresion
    
    * update bootstrap
    
    * Added e2e test
    
    * load secrets on pipelines
    
    * source target update
    
    * Fix pipeline vault
    
    * Fix query
    
    * Fix db url config
    
    * Fix query
    
    * removed not set vars
    
    * Fix test query
    
    * Upload all case data
    
    * Extend e2e test
    
    * Improve db connection
    
    * Fix coverage
    
    * Fix cs
    
    * Reverted db connection changes
    
    * Updated ignore annotation

---
+++ src/e2e/java/uk/gov/hmcts/reform/dataextractor/functional/postdeploy/BlobReader.java
@@ -0,0 +1,43 @@
+package uk.gov.hmcts.reform.dataextractor.functional.postdeploy;
+
+import com.azure.storage.blob.BlobClient;
+import com.azure.storage.blob.BlobServiceClient;
+import com.azure.storage.blob.BlobServiceClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import uk.gov.hmcts.reform.dataextractor.WriterException;
+
+import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
+
+@Component
+public class BlobReader {
+
+    private final String connectStr;
+
+    public BlobReader(@Value("${etl.connection-string}") String connectStr) {
+        this.connectStr = connectStr;
+    }
+
+    protected BlobServiceClient getBlobServiceClient() {
+        try {
+            return new BlobServiceClientBuilder().connectionString(connectStr).buildClient();
+        } catch (IllegalArgumentException e) {
+            throw new WriterException(e);
+        }
+    }
+
+    public String  readFile(String container, String fileName) {
+        OutputStream output = new ByteArrayOutputStream();
+
+        BlobServiceClient blobContainer = getBlobServiceClient();
+        BlobClient blobClient = blobContainer.getBlobContainerClient(container).getBlobClient(fileName);
+        blobClient.download(output);
+        return output.toString();
+    }
+
+    public BlobClient  getBlobClient(String container, String fileName) {
+        BlobServiceClient blobContainer = getBlobServiceClient();
+        return blobContainer.getBlobContainerClient(container).getBlobClient(fileName);
+    }
+}
---
+++ src/e2e/java/uk/gov/hmcts/reform/dataextractor/functional/postdeploy/DataExtractorTest.java
@@ -0,0 +1,96 @@
+package uk.gov.hmcts.reform.dataextractor.functional.postdeploy;
+
+import com.azure.storage.blob.BlobClient;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import uk.gov.hmcts.reform.dataextractor.DataExtractorApplication;
+import uk.gov.hmcts.reform.dataextractor.QueryExecutor;
+import uk.gov.hmcts.reform.dataextractor.config.DbConfig;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.stream.Stream;
+
+import static java.time.ZoneOffset.UTC;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:application_e2e.properties")
+@SpringBootTest
+public class DataExtractorTest {
+    static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+    @Autowired
+    private DbConfig dbConfig;
+
+    @Autowired
+    private BlobReader blobReader;
+
+    private static final  String SQL_QUERY_BY_CASETYPE = "SELECT count(*) \n"
+        + "FROM case_event CE\n"
+        + "WHERE CE.case_type_id = '%s'\n"
+        + "AND created_date >= (current_date-1 + time '00:00')\n"
+        + "AND created_date < (current_date + time '00:00')";
+
+
+    static Stream<Arguments> caseTypeInfoProvider() {
+        return Stream.of(
+            arguments("probate", "GrantOfRepresentation", DataExtractorApplication.Output.JSON_LINES, "PROB-GOR"),
+            arguments("divorce", "divorce", DataExtractorApplication.Output.JSON_LINES, "DIV")
+        );
+    }
+
+    @ParameterizedTest(name = "#{index} - Testing caseType: {1}")
+    @MethodSource("caseTypeInfoProvider")
+    @Disabled // Do not verify DB content
+    public void checkByCaseType(String container, String caseType, DataExtractorApplication.Output type, String prefix) {
+        String sqlQuery = getQueryByCaseType(caseType);
+        String fileName = getFileName(prefix, type);
+        QueryExecutor queryExecutor = new QueryExecutor(dbConfig.getUrl(), dbConfig.getUser(), dbConfig.getPassword(), sqlQuery);
+        try {
+            ResultSet resultSet = queryExecutor.execute();
+            resultSet.next();
+            int rows = resultSet.getInt(1);
+            String blobContent = blobReader.readFile(container, fileName);
+            assertTrue(rows > 0, "Rows numbers " + rows);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                queryExecutor.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @ParameterizedTest(name = "#{index} - Testing caseType: {1}")
+    @MethodSource("caseTypeInfoProvider")
+    public void checkBlobAreCreated(String container, String caseType, DataExtractorApplication.Output type, String prefix) {
+        String fileName = getFileName(prefix, type);
+
+        BlobClient blobClient = blobReader.getBlobClient(container, fileName);
+        assertTrue(blobClient.exists(), "Blob missing");
+        assertTrue(blobClient.getProperties().getBlobSize() > 0, "Blob empty");
+    }
+
+    public String getFileName(String prefix, DataExtractorApplication.Output extension) {
+        return String.format("%s-%s.%s", prefix, DATE_TIME_FORMATTER.format(LocalDateTime.now(ZoneId.from(UTC))),
+            extension.getExtension());
+    }
+
+    private String getQueryByCaseType(String caseType) {
+        return String.format(SQL_QUERY_BY_CASETYPE, caseType);
+    }
+}
